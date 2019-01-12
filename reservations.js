305bfef691a04c7ced21dff2917ef27cfd9c5f68
KOV <43220938+kwitherington@users.noreply.github.com>
2019-01-12 11:03:46 -0600

    reservation pages added

---
+++ reservations.js
@@ -0,0 +1,35 @@
+
+$(".submit").on("click", function(event) {
+    event.preventDefault();
+
+    var newReservation = {
+      customerName: $("#nameInput").val().trim(),
+      phoneNumber: $("#phoneInput").val().trim(),
+      customerEmail: $("#emailInput").val().trim(),
+      customerID: $("#uniqueID").val().trim()
+    };
+
+    console.log(newReservation);
+
+    $.post("/api/tables", newReservation,
+      function(data) {
+
+        // If a table is available, tell user they are booked.
+        if (data) {
+          alert("Yay! You are officially booked!");
+        }
+
+        // If a table is available, tell user they on the waiting list.
+        else {
+          alert("Sorry you are on the wait list");
+        }
+
+        // Clear the form when submitting
+        $("#nameInput").val("");
+        $("#phoneInput").val("");
+        $("#emailInput").val("");
+        $("#uniqueID").val("");
+
+    });
+
+});
